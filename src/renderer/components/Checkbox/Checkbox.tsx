aabdd2e1b86fcf75e29938e60c5b04dd7c16a5ad
adcimon <adrian.ciborro.montes@gmail.com>
2023-01-31 14:14:17 +0100

    Updated checkbox style

--- src/renderer/components/Checkbox/Checkbox.tsx
+++ src/renderer/components/Checkbox/Checkbox.tsx
@@ -19,7 +19,7 @@ export class Checkbox extends React.Component<ICheckboxProps, ICheckboxState>
 					<label className='flex items-center text-base-400'>
 						<input
 							type='checkbox'
-							className='text-primary-600 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary-600 form-checkbox'/>
+							className='inline-block w-4 h-4 text-primary-600 bg-base-700 border-1 rounded border-base-600 focus:border-primary-400 focus:outline-none focus:shadow-outline-primary-600 checkbox'/>
 						<span
 							className='ml-2'>
 							{this.props.children}
