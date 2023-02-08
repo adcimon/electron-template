e348f70bdb22b07160b48564fc36577b3fdc9ec1
adcimon <adrian.ciborro.montes@gmail.com>
2023-02-08 13:31:02 +0100

    Implemented multiselect component

--- src/renderer/components/Multiselect/Multiselect.tsx
+++ src/renderer/components/Multiselect/Multiselect.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-interface ISelectProps
+interface IMultiselectProps
 {
 	label?: string;
 	value?: string | number | readonly string[] | undefined;
@@ -9,11 +9,11 @@ interface ISelectProps
 	children?: React.ReactNode;
 }
 
-interface ISelectState
+interface IMultiselectState
 {
 }
 
-export class Select extends React.Component<ISelectProps, ISelectState>
+export class Multiselect extends React.Component<IMultiselectProps, IMultiselectState>
 {
 	public render = () =>
 	{
@@ -26,9 +26,10 @@ export class Select extends React.Component<ISelectProps, ISelectState>
 						{this.props.label}
 					</span>
 					<select
+						multiple={true}
 						value={this.props.value}
 						onChange={this.props.onChange}
-						className={this.props.className + ' block mt-1 px-3 py-2 text-sm text-base-300 bg-base-700 border-1 rounded border-base-600 hover:border-primary-500 focus:border-primary-600 focus:outline-none focus:shadow-outline-purple focus:shadow-outline-primary-600 transition-colors duration-150 select'}>
+						className={this.props.className + ' block mt-1 px-3 py-2 text-sm text-base-300 bg-base-700 border-1 rounded border-base-600 hover:border-primary-500 focus:border-primary-600 focus:outline-none focus:shadow-outline-purple focus:shadow-outline-primary-600 transition-colors duration-150'}>
 						{this.props.children}
 					</select>
 				</label>
