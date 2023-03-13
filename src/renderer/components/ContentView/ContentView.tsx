c0cbde8c2a8064477643c0d7b87c76438cbbbbe7
adcimon <adrian.ciborro.montes@gmail.com>
2023-03-13 15:02:41 +0100

    Implemented icon components

--- src/renderer/components/ContentView/ContentView.tsx
+++ src/renderer/components/ContentView/ContentView.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Card } from '../Core/Card/Card';
 import { DataCard } from '../Core/Card/DataCard';
+import { UserGroupSolid } from '../Core/Icon/UserGroupSolid';
+import { TrophySolid } from '../Core/Icon/TrophySolid';
+import { RocketLaunchSolid } from '../Core/Icon/RocketLaunchSolid';
+import { Square2StackSolid } from '../Core/Icon/Square2StackSolid';
 
 interface IContentViewProps
 {
@@ -22,41 +27,38 @@ export class ContentView extends React.Component<IContentViewProps>
 						className='my-6 text-2xl font-semibold text-base-200'>
 						Content
 					</h1>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
-					</DataCard>
-					<DataCard title='Data Card'>
-						666$
+					<Card
+						title='Lorem ipsum'>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</Card>
+					<div
+						className='flex items-center justify-center flex-wrap gap-3'>
+						<DataCard
+							title='Users'
+							icon={<UserGroupSolid/>}
+							className='flex-1'>
+							9
+						</DataCard>
+						<DataCard
+							title='Trophies'
+							icon={<TrophySolid/>}
+							className='flex-1'>
+							11
+						</DataCard>
+						<DataCard
+							title='Rockets'
+							icon={<RocketLaunchSolid/>}
+							className='flex-1'>
+							100
+						</DataCard>
+					</div>
+					<DataCard
+						title='Data Cards'
+						icon={<Square2StackSolid/>}>
+						4
 					</DataCard>
 				</div>
 			</>
